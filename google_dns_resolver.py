63b0015041c2934e86d887fa56ce59a243c290e2
Aman Kumar <amankumar.msjps@gmail.com>
2018-02-28 10:43:35 +0530

    argument is added

--- google_dns_resolver.py
+++ google_dns_resolver.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
 by Kumar Aman
 
 """
-
+import sys
 import socket
 
-server = input("Provide the domain address to resolve: ")
-
+if (len(sys.argv) < 2):
+    server = input("Provide the domain address to resolve: ")
+else:
+    server = sys.argv[1]
+    
 print("IP: ", socket.gethostbyname(server))
