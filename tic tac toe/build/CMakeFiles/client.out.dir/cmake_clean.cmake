7de41eccd2008d65233fceef9ef41aebd5228746
Aman Kumar <amankumar.msjps@gmail.com>
2018-03-27 17:38:03 +0530

    CMakeLists.txt added for compile

---
+++ tic tac toe/build/CMakeFiles/client.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.out.dir/game_client.c.o"
+  "client.out.pdb"
+  "client.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/client.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tic tac toe/build/CMakeFiles/server.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.out.dir/game_server.c.o"
+  "server.out.pdb"
+  "server.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
